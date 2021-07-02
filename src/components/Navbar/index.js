5746e32ff3d89aab3c19f2f375bcf73edb821e82
ajiiz <opticpiter@gmail.com>
2021-07-02 13:59:06 +0200

    Added Navbar elements

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
-const Logo = "testlogo"
+import Logo from "../../assets/logo.svg"
 
 const index = () => {
     return (
-        <div className="navbar">
-            <Logo />
-
+        <div className="navbar" style={{backgroundColor:"black"}}>
+            <img src={Logo}></img>
+            <ul className="nav-items">
+                <li className="nav-items__items">
+                    <Link className="nav-items__link" to="/home">home</Link>
+                </li>
+                <li className="nav-items__items">
+                    <Link className="nav-items__link" to="/shop">shop</Link>
+                </li>
+                <li className="nav-items__items">
+                    <Link className="nav-items__link" to="/about">about</Link>
+                </li>
+                <li className="nav-items__items">
+                    <Link className="nav-items__link" to="/contact">contact</Link>
+                </li>
+            </ul>
         </div>
     )
 }
