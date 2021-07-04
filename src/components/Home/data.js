d001fcce7a4742e15d4f917762b072fd84048d86
ajiiz <opticpiter@gmail.com>
2021-07-04 12:22:13 +0200

    Added home component data file

---
+++ src/components/Home/data.js
@@ -0,0 +1,31 @@
+import HeroImage1 from "../../assets/desktop-image-hero-1.jpg"
+import HeroImage2 from "../../assets/desktop-image-hero-2.jpg"
+import HeroImage3 from "../../assets/desktop-image-hero-3.jpg"
+
+const data = {
+    galleryData: [
+        {
+            image: HeroImage1,
+            header: "Discover innovative ways to decorate",
+            content: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+        },
+        {
+            image: HeroImage2,
+            header: "We are avaible all across the globe",
+            content: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
+        },
+        {
+            image: HeroImage3,
+            header: "Manufuctured with the best materials",
+            content: "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+        },
+    ],
+
+    aboutData:
+        {
+            header: "About our furniture",
+            content: "Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space."
+        }
+}
+
+export default data
