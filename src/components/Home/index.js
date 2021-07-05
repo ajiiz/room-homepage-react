b7d54b3c91df93c5fe7a2e63c9a12dcb9f2a39e9
ajiiz <opticpiter@gmail.com>
2021-07-05 23:57:46 +0200

    Added methods to change slides

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Navbar from "../Navbar"
 import data from "./data"
@@ -8,11 +8,30 @@ import ArrowLong from "../../assets/icon-arrow.svg"
 import "../../styles/home.scss"
 
 const Home = () => {
+
+    const [currentSlide, setCurrentSlide] = useState([])
+    const [slideId, setSlideId] = useState(0)
+
+    const handleClick = (dir) => {
+        let dataLength = data.galleryData.length
+        let id = slideId
+        if (dir === "right") {
+            id = (slideId + 1 < dataLength) ? id + 1 : 0
+        } else if (dir === "left") {
+            id = (slideId - 1 >= 0) ? id - 1 : dataLength - 1
+        }
+        setSlideId(id)
+    }
+
+    useEffect(() => {
+        setCurrentSlide(data.galleryData[slideId])
+    }, [slideId])
+
     return (
         <>
             <Navbar />
             <div className="home">
-                <img className="home-gallery" src={data.galleryData[0].image}></img>
+                <img className="home-gallery" src={data.galleryData[slideId].image}></img>
                 <div className="home-info-wrapper">
                     <div className="home-info-text-container">
                         <div className="home-info-text">
@@ -25,10 +44,10 @@ const Home = () => {
                         </div>
                     </div>
                     <div className="home-info-arrows">
-                        <div className="home-info-arrows__arrow">
+                        <div className="home-info-arrows__arrow" onClick={() => handleClick("left")}>
                             <img src={ArrowLeft} />
                         </div>
-                        <div className="home-info-arrows__arrow">
+                        <div className="home-info-arrows__arrow" onClick={() => handleClick("right")}>
                             <img src={ArrowRight} />
                         </div>
                     </div>
