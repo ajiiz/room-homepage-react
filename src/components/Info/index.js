18fff21f864f58c6275758a03479fbd951a05190
ajiiz <opticpiter@gmail.com>
2021-07-08 12:31:51 +0200

    Added btn and handleClick method that pushes back to /home

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 
 import "../../styles/info.scss"
 
 const Info = ({ name }) => {
+
+    const history = useHistory()
+
+    const handleClick = (event) => {
+        event.preventDefault()
+        history.push("/home")
+    }
+
     return (
         <div className="info">
             <h1 className="info-text">There will be {name} component soon (probably...)</h1>
+            <button className="info-btb btn" onClick={handleClick}>go back</button>
         </div>
     )
 }
