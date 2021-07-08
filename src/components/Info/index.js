21c4ed6c5de7b8a56c37acfe07715a3ad788995d
ajiiz <opticpiter@gmail.com>
2021-07-08 12:10:48 +0200

    Added Information component

---
+++ src/components/Info/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = ({ name }) => {
+    return (
+        <div className="info">
+            <h1 className="info-text">There will be {name} component soon (probably...)</h1>
+        </div>
+    )
+}
+
+export default Info
